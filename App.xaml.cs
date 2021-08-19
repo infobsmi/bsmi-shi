c70e692ad68f69fe5b50f0069fe2a95ac1a129f5
tesw yew isal <278153+cnmade@users.noreply.github.com>
2021-08-19 09:47:09 +0800

    切换为.net 6和 WebView2

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,5 +1,4 @@
-﻿using CefSharp;
-using CefSharp.Wpf;
+﻿
 using System;
 using System.Collections.Generic;
 using System.Configuration;
@@ -17,23 +16,6 @@ namespace bsmi_shi
     {
         public App()
         {
-          //  var settings = new CefSettings();
-       //     settings.CefCommandLineArgs.Add("disable-gpu", "1");
-          //  settings.CefCommandLineArgs.Add("disable-gpu-compositing", "1");
-          //  settings.CefCommandLineArgs.Add("enable-begin-frame-scheduling", "0");
-
-           // settings.CefCommandLineArgs.Add("disable-gpu-vsync", "1"); //Disable Vsync
-
-            //Disables the DirectWrite font rendering system on windows.
-            //Possibly useful when experiencing blury fonts.
-         //   settings.CefCommandLineArgs.Add("disable-direct-write", "1");
-//
-
-          //  if (!Cef.IsInitialized)
-           // {
-                //Perform dependency check to make sure all relevant resources are in our output directory.
-           //     Cef.Initialize(settings, performDependencyCheck: true, browserProcessHandler: null);
-//}
         }
     }
 }
